b0ad435343072e3f92236d3f9144b8dcaf2c5a40
Axel Goré <105491625+Heidge@users.noreply.github.com>
2025-05-05 14:20:49 +0200

    Simple shell 0.1

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # holbertonschool-simple_shell
+
+Un mini shell en C capable d'exécuter des commandes comme `ls`, `cd`, `exit`, etc.
+
+## Commandes internes :
+- `cd [dir]` : changer de dossier
+- `exit` : quitter le shell
+- `help` : afficher l’aide
+
+## Compilation :
+```bash
+make
+```
+
+## Flowchart
+```mermaid
+flowchart TD
+    1((Démarrage du shell.c)) --> 2[Affiche #cisfun$]
+    2 --> 3[Recupere l'entree]
+    3 -->|Commande entree| 3A{Commande 'exit' ?}
+    3A -->|Oui| 3B((Sortir de shell.c))
+    3A -->|Non| 4{Commande du shell ?}
+    4 -->|Oui| 5A[Executer la commande shell] -->|Boucle infinie| 2
+    4 -->|Non| 5B[Chercher la commande dans 'PATH']
+    5B --> 5C{Commande repertoriee ?}
+    5C -->|Oui| 6A[Executer la commande repertoriee] -->|Boucle infinie| 2
+    5C -->|Non| 6B[Erreur execve] -->|Boucle infinie| 2
+```
---
+++ shell.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
+/*protos*/
+void display_prompt(void);
+void execute_command(char *line, char **argv);
+
+/**
+* main - boucle principale et affichage du prompt
+* @argc: compteur d'argument (non utilisé ici)
+* @argv: vecteur d'argument (contien nom du programme ou arguments
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	(void)argc; /*argc n'est pas utilisé donc on le mute*/
+
+	while (1)
+		{
+			display_prompt();
+			nread = getline(&line, &len, stdin);
+
+			if (nread == -1)
+				{
+				printf("\n");
+				break;
+				}
+
+			line[nread - 1] = '\0'; /*supprime le \n en fin de commande*/
+
+			if (strcmp(line, "exit") == 0)
+				break;
+
+			execute_command(line, argv);
+		}
+
+		free(line);
+		return (0);
+}
+
+/**
+* display_prompt - affiche le shell prompt
+*/
+
+void display_prompt(void)
+{
+	printf("($)");
+}
+
+/**
+* execute_command - crée les forks et execute les commandes
+* @line: entrée utilisateur
+* @argv: vecteur d'argument de la fonction main
+*/
+
+void execute_command(char *line, char **argv)
+{
+	pid_t pid;
+	int status;
+	char *cmd_argv[2];
+
+	if (line[0] == '\0')
+		return;
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("erreur fork");
+		return;
+	}
+
+	if (pid == 0)
+	{
+		if (access(line, X_OK) == -1)
+		{
+			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+
+		cmd_argv[0] = line;
+		cmd_argv[1] = NULL;
+
+		if (execve(line, cmd_argv, environ) == -1)
+		{
+			fprintf(stderr, "%s: No such file or directory\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		wait(&status);
+}
